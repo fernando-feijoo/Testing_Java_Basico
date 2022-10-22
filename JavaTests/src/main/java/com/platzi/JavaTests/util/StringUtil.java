ccc77794a1b40632e9e3f83f76f8569937a50d4c
PC ESCRITORIO <fernando95f4@gmail.com>
2022-10-22 11:26:00 -0500

    Actualizacion del proyecto con nuevas clases y tests.

--- JavaTests/src/main/java/com/platzi/JavaTests/util/StringUtil.java
+++ JavaTests/src/main/java/com/platzi/JavaTests/util/StringUtil.java
@@ -2,7 +2,13 @@ package com.platzi.JavaTests.util;
 
 public class StringUtil {
     public static String repeat(String str, int times) {
+
+        if (times < 0){
+            throw new IllegalArgumentException("Negative times not allowed.");
+        }
+
         String result = "";
+
         for (int i = 0; i < times; i++) {
             result += str;
         }
