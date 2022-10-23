471bd45108efda729db6514e4e3b608e9b35efc8
PC PORTATIL <fernando95f4@gmail.com>
2022-10-23 14:13:29 -0500

    Actualización y commit directo.

--- JavaTests/src/main/java/com/platzi/JavaTests/util/DateUtil.java
+++ JavaTests/src/main/java/com/platzi/JavaTests/util/DateUtil.java
@@ -1,6 +1,7 @@
 package com.platzi.JavaTests.util;
 
 public class DateUtil {
+//    Nueva linea de codigo, para prueba de Commit directo de InteligentIDEA
     public static boolean isLeapYear(int year) {
 
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
